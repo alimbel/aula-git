18d048231cefb402a0b9e06c2201fc3d96ea661f
04238295218 <04238295218@JIPA-LABIN02-23.ifro.local>
2018-05-21 14:42:38 -0400

    menu tela principal

--- 3B_Alimbel/src/br/edu/ifro/MenuPrincipalController.java
+++ 3B_Alimbel/src/br/edu/ifro/MenuPrincipalController.java
@@ -5,13 +5,19 @@
  */
 package br.edu.ifro;
 
+import com.jfoenix.controls.JFXButton;
 import java.net.URL;
 import java.util.ResourceBundle;
 import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.Button;
+import javafx.scene.control.MenuItem;
 import javafx.scene.control.TextField;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+import java.io.IOException;
 
 
 /**
@@ -24,9 +30,12 @@ public class MenuPrincipalController implements Initializable {
     private TextField txtNumero1, txtNumero2, txtResultado;
     
     @FXML
-    private Button btnSomar;
-    
+    private JFXButton btnSoma;
     @FXML
+    private MenuItem Cadastrar;
+    @FXML
+    private MenuItem fechar;
+    
     private void soma(ActionEvent event) {
         Double num1 = Double.parseDouble(txtNumero1.getText());
         Double num2 = Double.parseDouble(txtNumero2.getText());
@@ -40,5 +49,41 @@ public class MenuPrincipalController implements Initializable {
     public void initialize(URL url, ResourceBundle rb) {
         // TODO
     }    
+
+    @FXML
+    private void txtNumero1(ActionEvent event) {
+    }
+
+    @FXML
+    private void txtNumero2(ActionEvent event) {
+    }
+
+    @FXML
+    private void btnSomar(ActionEvent event) {
+    }
+
+    @FXML
+    private void txtResultado(ActionEvent event) {
+    }
+
+    @FXML
+    private void cadastrarAluno(ActionEvent event) {
+        try{
+    FXMLLoader fxmlLoader = new FXMLLoader();
+    fxmlLoader.setLocation(getClass().getResource("Alimbel.fxml"));
+    Scene scene = new Scene(fxmlLoader.load(),900,682);
+    Stage stage = new Stage();
+    stage.setTitle("Cadastrar Alimbel");
+    stage.setScene(scene);
+    stage.show();
+}
+catch(IOException e){
+
+}
+    }
+
+    @FXML
+    private void fecharProjeto(ActionEvent event) {
+    }
     
 }
